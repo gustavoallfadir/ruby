3c493357353e60740923ed996c76227a57544a20
Gustavo Allfadir <gustavo.allfadir@gmail.com>
2021-01-22 00:51:24 -0600

    null

--- backend/app.rb
+++ backend/app.rb
@@ -7,15 +7,15 @@ app = Proc.new do
 		[	
 			'<head> <meta charset="UTF-8"> </head>',
 
-			"<h1>Hello world!, The time is #{Time.now}</h1>",
+			"<h1>Hello world!, The time is #{Time.now.strftime("%d/%b/%Y, %I:%M %p")}</h1>",
 
-			"<p>Bienvenido a este sitio de prueba, sólo es un backend sencillo con Ruby</p>",
+			"<h3>Bienvenido a este sitio de prueba, sólo es un backend sencillo con Ruby</h3>",
 			
 			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-				non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
 		],
 	]
 end
